b095c29a8f573eacb0591543e8922b2b07797045
leshaze <n.hilse@gmx.de>
2022-01-27 22:19:08 +0100

    store and edit record

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.app')
-
+@section('title',' - Dashboard')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -8,20 +8,26 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    {{-- @if (session('status'))
+                    @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif --}}
+                    @endif
 
-                    <p class="font-weight-normal">
-                        Dashboard
-                        {{-- Aktuelle Einträge: <b>{{ $count }}</b><br>
-                        Aktuelle Summe aller Einträge: <b>{{ $total_value }} €</b><br>
-                        Aktuelle Anzahl von CDs: <b>{{ $count_cd }}</b><br>
-                        Aktuelle Anzahl von LPs: <b>{{ $count_lp }}</b><br>
-                        Aktuelle Anzahl von Künstlern: <b>{{ $count_artist }}</b><br>
-                        Aktuelle Anzahl von Labeln: <b>{{ $count_label }}</b> --}}
+                    <p class="font-weight-normal">                      
+                        @auth
+                        @if($count)
+                            Aktuelle Einträge: <b>{{ $count }}</b><br>
+                            Aktuelle Summe aller Einträge: <b>{{ $total_value }} €</b><br>
+                            Aktuelle Anzahl von CDs: <b>{{ $count_cd }}</b><br>
+                            Aktuelle Anzahl von LPs: <b>{{ $count_lp }}</b><br>
+                            Aktuelle Anzahl von Künstlern: <b>{{ $count_artist }}</b><br>
+                            Aktuelle Anzahl von Labeln: <b>{{ $count_label }}</b>  
+                        @else 
+                        There is no data yet.
+                        @endif 
+                        @endauth
+                        
                     </p>
                 </div>
             </div>
