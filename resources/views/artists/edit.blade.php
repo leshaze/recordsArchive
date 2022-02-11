b703cfa13c1fe2d70804d75c911542a121682153
leshaze <leshaze@tutanota.com>
2022-02-11 22:39:32 +0100

    ArtistController and views

--- resources/views/artists/edit.blade.php
+++ resources/views/artists/edit.blade.php
@@ -1,23 +1,34 @@
-@extends('layout.main')
-@section('title','Edit Artist')
+@extends('layout.app')
+@section('title','- Edit Artist')
 @section('content')
-<br><h2 class="offset-sm-1">Edit Artist</h2>
-<div class="row mt-2">
-  <div class="col-sm-4 offset-sm-1">
-      <form action="{{route('artist.update')}}" method = "post">
-        @csrf
-        <div class="form-label-group">
-          <input type="text" name="name" id="name"  class="form-control" placeholder="" autofocus="" value="{{$artist->name}}">
-          <label for="archive_number">Artist</label>
-        </div>
-      <div class="col-sm-4form-row">
-        <div class="form-label-group">
-          <textarea class="form-control" name="description" id="description" placeholder="Beschreibung" autofocus="" rows="4">{{$artist->description}}</textarea>
-        </div>
+<div class="container">
+  <div class="wrapper flex" id="artistCreate">
+      <div class="card" id="artistCreate">
+          <div class="card-header">{{ __('Create Artist') }}</div>
+          <form action="{{ route('artists.store', ['artist' => $artist->id]) }}" method="post" class="Artist">
+            @method('PUT')
+              @csrf
+              <p>
+                  <label for="title">Künstler</label> 
+                  @error('name')
+                  <span class="text-danger">
+                      {{ $message }}
+                  </span>
+              @enderror
+              <input type="text" name="name" id="name"
+                  class="form-control  @error('name') border border-danger @enderror" placeholder="Künstler"
+                  autofocus="" value="{{ $artist->name }}">
+              </p>
+              <p></p>
+              <p></p>
+                <p class="full-width">
+                  <textarea class="form-control" name="description" id="description" placeholder="Beschreibung" autofocus=""
+                      rows="3">{{ $artist->description }}</textarea>
+              </p>
+
+              <button style="min-width:70px; max-width:90px" type="submit" class="btn btn-success">Submit</button>
+          </form>
       </div>
-        <input type="hidden" name="id" value = "{{$artist->id}}">
-        <button type = "submit" class = "btn btn-success">Submit</button>
-      </form>
-    </div>
   </div>
+</div>
 @endsection
