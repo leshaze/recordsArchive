f190197ac3e4eb62ef45d25f2bbd58129d5b69f1
leshaze <leshaze@tutanota.com>
2022-03-06 14:27:50 +0100

    custom 503

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -13,6 +13,5 @@
     <h1>We&rsquo;ll be back soon!</h1>
     <div>
         <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. We&rsquo;ll be back online shortly!</p>
-        <p>&mdash; The Team</p>
     </div>
 </article>
